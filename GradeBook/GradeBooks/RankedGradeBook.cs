f2fbbf8d4c3a1de5ef8eee95f9b3d2f8e290ce11
telviaczek <xtelvi@gmail.com>
2024-03-16 11:34:03 +0100

    RankedGradeBook class created.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook:BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+    }
+}
