3c0d4d8a223622e881e14a3f0981612f0b888556
telviaczek <xtelvi@gmail.com>
2024-03-16 11:43:17 +0100

    Implement custom logic for CalculateStatistics method in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,21 @@ using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook:BaseGradeBook
+    internal class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else {
+                base.CalculateStatistics();
 
+            }
+        }
     }
 }
