0312eb087fbdcddcc463e5129fb93510c2a07079
telviaczek <xtelvi@gmail.com>
2024-03-16 11:30:56 +0100

    StandardGradeBook class created.

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook:BaseGradeBook
+    {
+        public StandardGradeBook(string name):base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+
+
+
+    }
+}
