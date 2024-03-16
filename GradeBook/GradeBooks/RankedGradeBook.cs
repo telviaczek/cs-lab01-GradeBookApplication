701d303a5d28eec3ab9e01158ec9f991f0dfee23
telviaczek <xtelvi@gmail.com>
2024-03-16 14:22:13 +0100

    Final commit with all features.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -24,6 +24,41 @@ namespace GradeBook.GradeBooks
             }
 
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var listOfGrades = Students.Select(x => x.AverageGrade).OrderByDescending(x=> x).ToList();
+            var top = listOfGrades.Count / 5f;
+            var highestCount = 0f;
+            var result = 5;
+            foreach (var grade in listOfGrades)
+            {
+                if (averageGrade >= grade) break;
+
+                highestCount++;
+                if (highestCount >= top)
+                {
+                    highestCount -= top;
+                    result--;
+                }
+
+            }
+            return result switch
+            {
+                5 => 'A',
+                4 => 'B',
+                3 => 'C',
+                2 => 'D',
+                _ => 'F'
+
+            };
+        }
+
+
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
